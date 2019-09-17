f99ae29048489b77a4755d014956f067de3b5ab5
stkoev <st.koev@outlook.com>
2019-09-17 18:10:33 +0300

    Section 10 end

---
+++ oop/get-set.js
@@ -0,0 +1,12 @@
+const data = {
+	locations: [],
+	get location() {
+		return this._location;
+	},
+	set location(value) {
+		this._location = value.trim();
+		this.locations.push(this._locations);
+	}
+};
+
+data.location = 'Philadelphia';
--- oop/person.js
+++ oop/person.js
@@ -1,28 +1,64 @@
 'use strict';
-const Person = function(firstName, lastName, age, likes = []) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.age = age;
-	this.likes = likes;
-};
-Person.prototype.getBio = function() {
-	let bio = `${this.firstName} is ${this.age}`;
-	this.likes.forEach((like) => {
-		bio += ` ${this.firstName} likes ${like}.`;
-	});
-	return bio;
-};
 
-Person.prototype.setName = function(fullName) {
-	const names = fullName.split(' ');
-	this.firstName = names[0];
-	this.lastName = names[1];
-};
+class Person {
+	constructor(firstName, lastName, age, likes = []) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.likes = likes;
+	}
+	getBio() {
+		let bio = `${this.firstName} is ${this.age}`;
+		this.likes.forEach((like) => {
+			bio += ` ${this.firstName} likes ${like}.`;
+		});
+		return bio;
+	}
+	set fullName(fullName) {
+		const names = fullName.split(' ');
+		this.firstName = names[0];
+		this.lastName = names[1];
+	}
+	get fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+class Employee extends Person {
+	constructor(firstName, lastName, age, position, likes) {
+		super(firstName, lastName, age, likes);
+		this.position = position;
+	}
+	getBio() {
+		return `${this.fullName} is a ${this.position}.`;
+	}
+	getYearsLeft() {
+		return 65 - this.age;
+	}
+}
+
+class Student extends Person {
+	constructor(firstName, lastName, age, grade, likes) {
+		super(firstName, lastName, age, likes);
+		this.grade = grade;
+	}
+	getBio() {
+		const status = this.grade >= 70 ? 'passing' : 'failing';
+		return `${this.firstName} is ${status} the exam.`;
+	}
+	updateGrade(amount) {
+		this.grade += amount;
+		return this.grade;
+	}
+}
 
 const me = new Person('Stefan', 'Koev', 41, [ 'reading', 'hiking', 'swimming' ]);
 me.setName('Koev Stefan');
 console.log(me);
 console.log(me.getBio());
 
-const person2 = new Person('Clancy', 'Turner', 34);
+const person2 = new Student('Clancy', 'Turner', 34, 85, [ 'dance', 'hike', 'read', 'play' ]);
 console.log(person2);
+console.log(person2.getBio());
+console.log(person2.updateGrade(-40));
+console.log(person2.getBio());
